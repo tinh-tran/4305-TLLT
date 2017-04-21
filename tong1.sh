befc19fe70d30777767a159f11f90cdfbf45dc13
tinhdk1 <jackymon41@gmail.com>
2017-04-21 13:59:03 +0700

    Create tong1.sh

---
+++ tong1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+echo “Chuong trinh tinh tong 1- $1” 
+index=0 
+tong=0 
+while [ $index -lt $1 ] 
+do 
+index=$(($index + 1)) 
+tong=$(($tong + $index)) 
+done 
+echo "Tong 1-$1= $tong" 
+exit 0 
+- Chạy chương trình : 
+chmod a+x tong1.sh 
+./tong1 100 
